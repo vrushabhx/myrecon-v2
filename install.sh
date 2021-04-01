052422264bb94a8694deac489ac0f02fdb2086e9
shubham chaskar <48474764+unstabl3@users.noreply.github.com>
2021-04-01 16:30:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ sudo apt install chromium-browser -y
 sudo apt install perl -y
 snap install jq
 snap install nmap
+apt install -y libpcap-dev
 source $HOME/.cargo/env
 
 if ! command -v pip2 &> /dev/null
