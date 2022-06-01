3e53a7958fe88a46582e2282e9ec6435cce32420
shubham chaskar <48474764+unstabl3@users.noreply.github.com>
2022-06-01 17:02:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -84,9 +84,7 @@ fi
 if ! command -v amass &> /dev/null
 then
 	echo -e "\e[92m[~] Not able to find amass..Installing..\e[00m\n"
-	go get -u github.com/OWASP/Amass/...
-	cd "$GOPATH"/src/github.com/OWASP/Amass/
-	go install ./...
+	snap install amass
 	cd /root/scripts/bounty/
 	echo -e "\e[93m[~] Configure API keys for better result..\e[00m\n"
 else
